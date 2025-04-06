bf6bb0ceae4d474000c7cee0965fca275d458c6f
tracer-ics2017 <tracer@njuics.org>
2025-04-06 23:03:30 +0800

    >  compile 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:03:30 up  5:02,  3 users,  load average: 1.01, 1.13, 1.00 1593dc6acd6c2178e26e98be9ca7de59afc10ca9

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -13,19 +13,22 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  int map_no;
-  if((map_no = is_mmio(addr)) >= 0) {
+  int map_no = is_mmio(addr);
+  if(map_no < 0) {
+    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  }
+  else{
     return mmio_read(addr, len, map_no);
   }
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  int map_no;
-  if((map_no = is_mmio(addr)) >= 0) {
+  int map_no = is_mmio(addr);
+  if(map_no < 0) {
+    memcpy(guest_to_host(addr), &data, len);
+  } else {
     mmio_write(addr, len, data, map_no);
   }
-  memcpy(guest_to_host(addr), &data, len);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
