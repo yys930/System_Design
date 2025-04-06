b4dd12c25a7f0240210be4b44d36c4f6bcfe008a
tracer-ics2017 <tracer@njuics.org>
2025-04-06 23:26:09 +0800

    >  run 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:26:09 up  5:24,  1 user,  load average: 0.66, 1.21, 1.36 a22effcb4e9af22499b58008c172482ab4023a7e

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -32,9 +32,10 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
 void _draw_sync() {
 }
 
+#define I8042_DATA_PORT 0x60
+#define I8042_STATUS_PORT 0x64
+
 int _read_key() {
-  #define I8042_DATA_PORT 0x60
-  #define I8042_STATUS_PORT 0x64
   if (inb(I8042_STATUS_PORT) == 1) {
     return inl(I8042_DATA_PORT);
   }
