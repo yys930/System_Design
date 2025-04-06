57503ee568e9a27983e0516c57870f3810ff3cf9
tracer-ics2017 <tracer@njuics.org>
2025-04-06 22:42:45 +0800

    >  compile 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:42:45 up  4:41,  3 users,  load average: 0.94, 0.56, 0.60 8dda7069f55f7011f4396900f7fc949a6b0ee14d

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -2,10 +2,11 @@
 
 make_EHelper(test) {
  //TODO();
-  rtl_and(&t2, &id_dest->val, &id_src->val);
-  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_and(&t0, &id_dest->val, &id_src->val);
+  
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
+  rtl_update_ZFSF(&t0, id_dest->width);
 
   print_asm_template2(test);
 }
