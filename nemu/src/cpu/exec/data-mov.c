1f724a9a89076dee832be4273e185cdb45b0df7c
tracer-ics2017 <tracer@njuics.org>
2025-04-07 19:12:57 +0800

    >  compile 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:12:57 up 31 min,  1 user,  load average: 0.25, 0.08, 0.13 691149dff51c988ebf3fa8d13e3cb92f3b29cdf8

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -41,7 +41,7 @@ make_EHelper(pusha) {
 
 make_EHelper(popa) {
   //TODO();
-  
+
   rtl_pop(&reg_l(R_EDI));
   rtl_pop(&reg_l(R_ESI));
   rtl_pop(&reg_l(R_EBP));
@@ -58,19 +58,23 @@ make_EHelper(popa) {
 
 make_EHelper(leave) {
   //TODO();
+
   rtl_mv(&reg_l(R_ESP), &reg_l(R_EBP));
   rtl_pop(&reg_l(R_EBP));
+
   print_asm("leave");
 }
 
 make_EHelper(cltd) {
   if (decoding.is_operand_size_16) {
     //TODO();
+
     rtl_sext(&t0, &reg_l(R_EAX), 2);
     rtl_shri(&reg_l(R_EDX), &t0, 16);
   }
   else {
     //TODO();
+
     rtl_sari(&reg_l(R_EDX), &reg_l(R_EAX), 31);
   }
 
@@ -80,6 +84,7 @@ make_EHelper(cltd) {
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
     //TODO();
+    
     rtl_shli(&reg_l(R_EAX), &reg_l(R_EAX), 24);
     rtl_sari(&reg_l(R_EAX), &reg_l(R_EAX), 8);
     rtl_shri(&reg_l(R_EAX), &reg_l(R_EAX), 16);
