ecae271800e6a4c5f56ebf165cef8e784c1644db
tracer-ics2017 <tracer@njuics.org>
2025-04-23 21:44:04 +0800

    >  compile 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:44:04 up 31 min,  1 user,  load average: 0.41, 0.18, 0.19 13a9b1eee7137847fc1a326932ba0cfaef32d9

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,13 @@
 #include "common.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
+void ramdisk_read(void *buf, off_t offset, size_t len);
+size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  //TODO();
+  size_t len;
+  len = get_ramdisk_size();
+  ramdisk_read(DEFAULT_ENTRY, 0, len);
   return (uintptr_t)DEFAULT_ENTRY;
 }
