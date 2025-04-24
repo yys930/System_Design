2b64b4e31e365c34530c5ead516c99f48ff9904d
tracer-ics2017 <tracer@njuics.org>
2025-04-24 19:28:26 +0800

    >  compile 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:28:26 up 53 min,  1 user,  load average: 0.26, 0.17, 0.19 c119df2666301d6e8b305a4a6dc1cfe350db83a0

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -41,12 +41,12 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  //TODO();
+  TODO();
   rtl_pop(&decoding.jmp_eip);
   decoding.is_jmp = 1;
   rtl_pop(&cpu.cs);
   rtl_pop(&cpu.eflags);
-  
+
   print_asm("iret");
 }
 
