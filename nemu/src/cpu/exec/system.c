8e6532f2139b6cf891aee0d60c6c42a5618e6586
tracer-ics2017 <tracer@njuics.org>
2025-04-24 20:09:52 +0800

    >  compile 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:09:52 up  1:34,  1 user,  load average: 0.55, 0.35, 0.27 733426b07ce38767c8e817e81395365d9683c925

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -33,6 +33,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr);
 make_EHelper(int) {
   //TODO();
   raise_intr(id_dest->val, decoding.seq_eip);
+  
   print_asm("int %s", id_dest->str);
 
 #ifdef DIFF_TEST
