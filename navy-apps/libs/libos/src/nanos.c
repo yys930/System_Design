75fdf91ade5f2835f24ed32e6d6a8d0db4bc7f62
tracer-ics2017 <tracer@njuics.org>
2025-04-26 17:14:20 +0800

    >  run 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:14:20 up 27 min,  1 user,  load average: 0.12, 0.21, 0.25 9b001c935a514499e01a06aaf19058c7b936dd

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -27,7 +27,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count){
   //_exit(SYS_write);
-  return _syscall_(SYS_write, fd, (uintptr_t)buf, count);
+  return _syscall_(SYS_write, (uintptr_t)fd, (uintptr_t)buf, (uintptr_t)count);
 }
 
 void *_sbrk(intptr_t increment){
