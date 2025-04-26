39df6e10387902306bff407dd39d545a904faa11
tracer-ics2017 <tracer@njuics.org>
2025-04-26 18:58:01 +0800

    >  run 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:58:01 up  2:11,  2 users,  load average: 0.04, 0.20, 0.22 6fde88b32e62bfb9ec43abab59f57227f7a0cb30

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,10 +4,18 @@
 void ramdisk_read(void *buf, off_t offset, size_t len);
 size_t get_ramdisk_size();
 
+int fs_open(const char *pathname, int flags, int mode);
+ssize_t fs_read(int fd, void *buf, size_t len);
+size_t fs_filesz(int fd);
+int fs_close(int fd);
+
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
-  size_t len;
-  len = get_ramdisk_size();
-  ramdisk_read(DEFAULT_ENTRY, 0, len);
+  // size_t len;
+  // len = get_ramdisk_size();
+  // ramdisk_read(DEFAULT_ENTRY, 0, len);
+  int fd = fs_open(filename, 0, 0);
+  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
