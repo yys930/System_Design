c07ce17a4ef86a2839bd5829c686ae485da4ddcb
tracer-ics2017 <tracer@njuics.org>
2025-05-19 20:58:18 +0800

    >  compile 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:58:18 up  1:47,  2 users,  load average: 0.70, 0.35, 0.27 2eeda7e2410a0b69de7c3842635f17123344

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -313,6 +313,7 @@ make_DHelper(out_a2dx) {
 void operand_write(Operand *op, rtlreg_t* src) {
   if (op->type == OP_TYPE_REG) { rtl_sr(op->reg, op->width, src); }
   else if (op->type == OP_TYPE_MEM) { rtl_sm(&op->addr, op->width, src); }
+  else if (op->type == OP_TYPE_CREG) { rtl_scr(op->reg, src); }
   else { assert(0); }
 }
 
