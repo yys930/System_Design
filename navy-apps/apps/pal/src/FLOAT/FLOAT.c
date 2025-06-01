26ee22778ccd8bd8ce5de8b9458e9c042dafea0e
tracer-ics2017 <tracer@njuics.org>
2025-06-01 21:44:45 +0800

    >  run 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:44:45 up  1:34,  2 users,  load average: 0.31, 0.66, 0.72 1a12a77d4493668ada9063d425f65d218d01b25d

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -104,7 +104,7 @@ FLOAT f2F(float a) {
 FLOAT Fabs(FLOAT a) {
   // assert(0);
   // return 0;
-  return a < 0 ? -a : a;
+  return (a & 0x80000000 == 0)? a : -a;
 }
 
 /* Functions below are already implemented */
