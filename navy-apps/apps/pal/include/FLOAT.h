84d47280520914a60865790efd8f781f686662de
tracer-ics2017 <tracer@njuics.org>
2025-06-01 20:46:24 +0800

    >  run 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:46:24 up 36 min,  1 user,  load average: 0.82, 0.92, 1.05 ae002c9ceef797c86af27d202bc4d95d379b29b6

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -18,13 +18,15 @@ static inline FLOAT int2F(int a) {
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
-  assert(0);
-  return 0;
+  // assert(0);
+  //return 0;
+  return a * b;
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
-  assert(0);
-  return 0;
+  // assert(0);
+  //return 0;
+  return a / b;
 }
 
 FLOAT f2F(float);
