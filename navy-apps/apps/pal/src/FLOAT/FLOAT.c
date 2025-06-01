c295f68e56311c32ee47ee1b3aebf20585eec705
tracer-ics2017 <tracer@njuics.org>
2025-06-01 22:27:06 +0800

    >  run 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:27:06 up  2:17,  2 users,  load average: 1.19, 0.71, 0.45 9e9d51f8a9b92bfba466cbc4e404fbd1c873f1df

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -3,39 +3,13 @@
 #include <assert.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  // assert(0);
-  // return 0;
-  return ((int64_t)a * (int64_t)b) >> 16;
+  assert(0);
+  return 0;
 }
 
-// FLOAT F_div_F(FLOAT a, FLOAT b) {
-//   // assert(0);
-//   // return 0;
-  
-//   // return ((int64_t)a << 16) / b;
-// }
-
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  // assert(0);
-  // return 0;
-  assert(b != 0);
-  FLOAT x = Fabs(a);
-  FLOAT y = Fabs(b);
-  FLOAT ret = x / y;
-  x = x % y;
-
-  for (int i = 0; i < 16; i++) {
-    x <<= 1;
-    ret <<= 1;
-    if (x >= y) {
-      x -= y;
-      ret++;
-    }
-  }
-  if (((a ^ b) & 0x80000000) == 0x80000000) {
-    ret = -ret;
-  }
-  return ret;
+  assert(0);
+  return 0;
 }
 
 FLOAT f2F(float a) {
@@ -49,62 +23,14 @@ FLOAT f2F(float a) {
    * performing arithmetic operations on it directly?
    */
 
-  // assert(0);
-  // return 0;
-  union float_ {
-    struct {
-      uint32_t man : 23;
-      uint32_t exp : 8;
-      uint32_t sign : 1;
-    };
-    uint32_t val;
-  };
-  union float_ f;
-  f.val = *((uint32_t*)(void*)&a);
-  int exp = f.exp - 127;
-  FLOAT ret = 0;
-  if (exp == 128)
-    assert(0);
-  if (exp >= 0) {
-    int mov = 7 - exp;
-    if (mov >= 0)
-      ret = (f.man | (1 << 23)) >> mov;
-    else
-      ret = (f.man | (1 << 23)) << (-mov);
-  }
-  else
-    return 0;
-  return f.sign == 0 ? ret : -ret;
-
-  // union {
-  //     float f;
-  //     uint32_t u;
-  //   } v;
-  //   v.f = a;
-
-  //   uint32_t sign = v.u >> 31;
-  //   int exp = ((v.u >> 23) & 0xFF) - 127;
-  //   uint32_t frac = (v.u & 0x7FFFFF) | 0x800000; // 1.f, 24位
-
-  //   int64_t val = (int64_t)frac;
-
-  //   if (exp >= 7) {
-  //     val = val << (exp - 7);
-  //   } else {
-  //     val = val >> (7 - exp);
-  //   }
-
-  //   val = val >> 8;  // Q7.24 -> Q16.16
-  //   if (sign) val = -val;
-
-  //   return (FLOAT)val;
+  assert(0);
+  return 0;
 
 }
 
 FLOAT Fabs(FLOAT a) {
-  // assert(0);
-  // return 0;
-  return (a > 0) ? a : -a;
+  assert(0);
+  return 0;
 }
 
 /* Functions below are already implemented */
