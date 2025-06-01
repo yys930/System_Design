2c8343e3091eeab594719bb272fd4293f4e14ff5
tracer-ics2017 <tracer@njuics.org>
2025-06-01 20:48:47 +0800

    >  run 2213781 song Linux song 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:48:47 up 38 min,  1 user,  load average: 0.55, 0.81, 0.99 7f35edcbd3b72cf149788003410928a6d2e02d9

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -3,8 +3,9 @@
 #include <assert.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+  return ((int64_t)a * b) >> 16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
